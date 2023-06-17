2f8667980cab2618db9ca06beb7731db8e21e2c2
jbarus <jakubbarus@gmail.com>
2023-06-17 14:01:21 +0200

    Basic functionalities have been added

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -5,16 +5,46 @@ public class ChatWindow extends JFrame {
     private JTextField messageTF;
     private JTextArea convTA;
     private JButton sendBT;
+    private JScrollPane scroller;
+
 
     public ChatWindow(){
         initWindow();
+        intiComponents();
     }
+
+    private void intiComponents() {
+        sendBT.setText("Send");
+
+        sendBT.addActionListener(e -> btnClicked());
+        messageTF.addActionListener(e -> btnClicked());
+
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+    }
+
     private void initWindow(){
+        convTA.setLineWrap(true);
+        convTA.setEditable(false);
+
         setTitle("Welcome");
         setSize(400,400);
         setLocationRelativeTo(null);
         setContentPane(panel);
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+
+        messageTF.requestFocus();
     }
+
+    private void btnClicked() {
+        if(!messageTF.getText().isEmpty()){
+            convTA.append(messageTF.getText()+"\n");
+            messageTF.setText("");
+        }
+        messageTF.requestFocus();
+    }
+
 }
